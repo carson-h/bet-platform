8b1a48e0961e8d2b08d3304d7a972dac8c9100c0
Carson Hall <51799808+carson-h@users.noreply.github.com>
2022-11-28 09:20:37 -0500

    Orientation determination added.

---
+++ aerial/orientation.ino
@@ -0,0 +1,58 @@
+#define alpha_pitch 0.0
+#define alpha_roll 0.0
+
+#define rad2deg 180/PI
+
+float accel_calib[] = {0.0, 0.0, 0.0};
+float gyro_calib[] = {0.0, 0.0, 0.0};
+
+float pitch;
+float roll;
+
+void calibrateIMU(int samples) {
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  for (int i = 0; i < samples; i++) {
+    lsm6ds3trc.getEvent(&accel, &gyro, &temp);
+    accel_calib[0] += accel.acceleration.x;
+    accel_calib[1] += accel.acceleration.y;
+    accel_calib[2] += accel.acceleration.z;
+    gyro_calib[0] += gyro.gyro.x;
+    gyro_calib[1] += gyro.gyro.y;
+    gyro_calib[2] += gyro.gyro.z;
+  }
+
+  accel_calib[0] /= samples;
+  accel_calib[1] /= samples;
+  accel_calib[2] /= samples;
+  gyro_calib[0] /= samples;
+  gyro_calib[1] /= samples;
+  gyro_calib[2] /= samples;
+}
+
+void initFilters() {
+  pitch = 0;
+  roll = 0;
+}
+
+float getPitch() {
+  return pitch;
+}
+
+float getRoll() {
+  return roll;
+}
+
+void updateFilters(sensors_event_t gyro, sensors_event_t accel, float dt) {
+  updatePitch(gyro, accel, dt);
+  updateRoll(gyro, accel, dt);
+}
+
+void updatePitch(sensors_event_t gyro, sensors_event_t accel, float dt) {
+  pitch = alpha_pitch * (getPitch() + gyro.gyro.y * dt) + (1 - alpha_pitch)*rad2deg*atan(accel.acceleration.x/accel.acceleration.z);
+}
+
+void updateRoll(sensors_event_t gyro, sensors_event_t accel, float dt) {
+  roll = alpha_roll * (getRoll() + gyro.gyro.x * dt)  + (1 - alpha_roll)*rad2deg*atan(accel.acceleration.y/accel.acceleration.z);
+}
